735109f458cede806b92c38e217d82d4ddf4a76b
raopremrao <raom61337@gmail.com>
2026-01-21 09:45:49 +0530

    Thrid Code

---
+++ Day1/03_Calculate_the_area_of_triangle_using_heroni_formula.py
@@ -0,0 +1,6 @@
+a = float(input("Enter 1st Side: "))
+b = float(input("Enter 2nd Side: "))
+c = float(input("Enter 3rd Side: "))
+s = ((a + b + c) / 2)
+area = (s * (s - a) * (s - b) * (s - c)) ** (0.5)
+print("Area of the trinagle is: " + str(area))
