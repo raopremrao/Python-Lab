8ca0a8fd40b01d3f70f06151971a5c194ec128ba
raopremrao <raom61337@gmail.com>
2026-01-21 09:38:05 +0530

    Second Code

---
+++ Day1/02_Calculate_the_distance_between_two_coordinates.py
@@ -0,0 +1,6 @@
+x1 = float(input("Enter the Value for X1: "))
+x2 = float(input("Enter the Value for X2: "))
+y1 = float(input("Enter the Value for Y1: "))
+y2 = float(input("Enter the Value for Y2: "))
+distance = (((x2 - x1)**2) + ((y2 - y1)**2)) ** (0.5)
+print("Distance: " + str(distance))
