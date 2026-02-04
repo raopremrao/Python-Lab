b084bee98543be59c25b9e837b6fedb3e2a609c7
raopremrao <raom61337@gmail.com>
2026-02-04 10:00:49 +0530

    Started Day2

---
+++ Day2/01.py
@@ -0,0 +1,23 @@
+list=[5,10,20,30,28]
+print(list)
+print(list[2])
+print(list[0:3])
+print(len(list))
+list[1]=40
+print(list)
+list.insert(1,78)
+print(list)
+list.append(89)
+print(list)
+print(max(list))
+print(min(list))
+list.sort()
+print(list)
+pls=[34,56,65]
+list.extend(pls)
+print(list)
+list.sort()
+print(list)
+list2=[123, 456, 789]
+list3= list + list2
+print(list3)
---
+++ Day2/02.py
@@ -0,0 +1,12 @@
+tuple1 = (2,584,451,7,8)
+print(len(tuple1))
+tuple2 = (2,5,8)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+print(max(tuple3))
+print(min(tuple3))
+print(tuple1 < tuple2)
+print(tuple1 > tuple2)
+print(tuple1 == tuple2)
+tuple4 = tuple1 * 2
+print(tuple4)
