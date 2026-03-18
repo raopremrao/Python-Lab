d41c8d89af5bda5e167287a52d54888ea5e1c8c3
raopremrao <raom61337@gmail.com>
2026-03-18 16:37:38 +0530

    Ended Exp 9

---
+++ Day6/Exp9/01.py
@@ -0,0 +1,12 @@
+file=open("loki.txt","r")
+print(file.read())
+file.close()
+
+
+# file=open("loki.txt","w")
+# file.write("best")
+# file.close()
+
+file=open("loki.txt","a")
+file.append("badddest")
+file.close()
